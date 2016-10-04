ede8967c4ea3de48097a83b6addaf487a5c3f577
Ken DeWitt <kenhedewitt@gmail.com>
2016-10-04 15:21:08 -0700

    formatting

--- js/script.js
+++ js/script.js
@@ -1,46 +1,43 @@
-(function() {
+(function () {
 	'use strict';
 
-	var output = document.getElementById( 'output' ),
-	closure = new myClosure2();
+	var output  = document.getElementById( 'output' ),
+	    closure = new myClosure2();
 
 	output.innerHTML = closure.foo();
 
-	window.setTimeout( function() {
+	window.setTimeout( function () {
 		output.innerHTML += '<br />' + closure.foo();
 	}, 500 );
-
-
-
-
 
 })();
 
 function nonClosure() {
 
-		var date = new Date();
-		return date.getMilliseconds();
+	var date = new Date();
+	return date.getMilliseconds();
 }
 
 function myClosure() {
 
 	var date = new Date();
 
-	return function() {
+	return function () {
 
 		return date.getMilliseconds();
 
-	}
+	};
 }
 
 function myClosure2() {
 
-	var date = new Date();
+	var date = new Date(),
+	    nestedFunc;
 
-	var nestedFunc = function() {
+	nestedFunc = function () {
 
 		return date.getMilliseconds();
-	}
+	};
 
 	return {
 		foo: nestedFunc
